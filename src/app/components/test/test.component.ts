7f011bb2fb743b1fe0575406bd627f5e5f8ee56d
paula <paulaxhindoli@gmail.com>
2022-03-21 15:14:11 +0100

    update formatting with prettier

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -3,52 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  styleUrls: ['./test.component.scss'],
 })
 export class TestComponent implements OnInit {
-selectedEmployee: any = null 
+  selectedEmployee: any = null;
 
-employees :any[]=[
-  {
-    employeeName : "Emma",
-    employeeSurname : "Smith",
-    employeeAge: 25,
-    employeeSalary: 2000,
-    employeePosition : "Developer"
-   },
-   {
-    employeeName : "John",
-    employeeSurname : "Doe",
-    employeeAge: 28,
-    employeeSalary: 2500,
-    employeePosition : "Developer"
-   },
-   {
-    employeeName : "Jane",
-    employeeSurname : "Smith",
-    employeeAge: 27,
-    employeeSalary: 2100,
-    employeePosition : "Developer"
-   },
-   {
-    employeeName : "Anna",
-    employeeSurname : "Delvery",
-    employeeAge: 27,
-    employeeSalary: 2100,
-    employeePosition : "Developer"
-   }
- ] 
+  employees: any[] = [
+    {
+      employeeName: 'Emma',
+      employeeSurname: 'Smith',
+      employeeAge: 25,
+      employeeSalary: 2000,
+      employeePosition: 'Developer',
+    },
+    {
+      employeeName: 'John',
+      employeeSurname: 'Doe',
+      employeeAge: 28,
+      employeeSalary: 2500,
+      employeePosition: 'Developer',
+    },
+    {
+      employeeName: 'Jane',
+      employeeSurname: 'Smith',
+      employeeAge: 27,
+      employeeSalary: 2100,
+      employeePosition: 'Developer',
+    },
+    {
+      employeeName: 'Anna',
+      employeeSurname: 'Delvery',
+      employeeAge: 27,
+      employeeSalary: 2100,
+      employeePosition: 'Developer',
+    },
+  ];
 
+  constructor() {}
 
-
-
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onShow(event: any, data: any) {
+    this.selectedEmployee = data;
   }
-onShow(event: any, data: any){
-  this.selectedEmployee = data;
-}
 }
